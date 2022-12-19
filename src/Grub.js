a319caa2b147195339c4c24f0f1e3a375ba22645
Kevin Le <iamkevinvle@gmail.com>
2022-12-19 10:39:38 -0800

    implement Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,11 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink'
+  this.food = 'jelly'
 };
 
+// Set Bee specific methods below:
+
+Grub.prototype.eat = function () {
+  return this.food;
+}
