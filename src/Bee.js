ff547546111f728de686c4041b418d5e1d914827
Kevin Le <iamkevinvle@gmail.com>
2022-12-19 10:40:25 -0800

    implement Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
 var Bee = function() {
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing'
 };
 
+Bee.prototype = new Grub(); //Bee prototype inherits from grub
+Bee.prototype.constructor = Bee;
+
+// Set Bee specific methods below:
