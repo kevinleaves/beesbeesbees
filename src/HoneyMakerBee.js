ebcdbeb77fb918aabc8f136173738ffdbb838a5f
Kevin Le <iamkevinvle@gmail.com>
2022-12-19 10:41:35 -0800

    implement HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+
+  this.age = 10;
+  this.job = 'make honey'
+  this.honeyPot = 0;
 };
 
+// HoneyMakerBee prototype inherits from Bee, which inherits from Grub
+HoneyMakerBee.prototype = new Bee();
+
+//set honeymakerbee prototype construction fn to be equal to itself?
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+//HoneyMakerBee specific methods
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++
+}
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--
+}
