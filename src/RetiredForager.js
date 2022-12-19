be5406ea52527fa4636be766495029435eeaa728
Kevin Le <iamkevinvle@gmail.com>
2022-12-19 10:57:18 -0800

    implement RetiredForager class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,21 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.color = 'grey'
+  this.canFly = false;
 };
 
+// set descendant prototype to a new instance of the parent class
+RetiredForagerBee.prototype = new ForagerBee();
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+// retiredForagerBee specific methods
+RetiredForagerBee.prototype.forage = function () {
+  return 'I am too old, let me play cards instead'
+}
+
+RetiredForagerBee.prototype.gamble = function (treasure) {
+  this.treasureChest.push(treasure)
+}
