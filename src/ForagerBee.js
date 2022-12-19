20722150263d63e0b2cca30c153c03bf3f774368
Kevin Le <iamkevinvle@gmail.com>
2022-12-19 10:46:59 -0800

    implement ForagerBee class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// to inherit, set descendent prototype to a new instance of the parent class
+ForagerBee.prototype = new Bee();
+
+// set decendent constructor equal to
+ForagerBee.prototype.constructor = ForagerBee;
+
+// forager bee specific methods
+ForagerBee.prototype.forage = function (treasure) {
+  this.treasureChest.push(treasure)
+}
